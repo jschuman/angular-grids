7db7a126a8160e22ac1a22f5d6650d3de0146563
Jeff Schuman <jschuman@solutionstreet.com>
2024-05-09 18:31:38 +0000

    ag grid up and running

---
+++ src/app/aggrid/aggrid.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
+import { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
+
+@Component({
+  selector: 'app-aggrid',
+  standalone: true,
+  imports: [AgGridAngular],
+  templateUrl: './aggrid.component.html',
+  styleUrl: './aggrid.component.css'
+})
+
+export class AggridComponent {
+  // Row Data: The data to be displayed.
+  rowData = [
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ];
+
+  CurrencyFormatter = (params: { value: any; }) => {
+    return `$${params.value}`;
+  }
+
+  columnTypes = {
+    currency: { 
+        width: 150,
+        valueFormatter: this.CurrencyFormatter
+    },
+};
+
+
+  // Column Definitions: Defines the columns to be displayed.
+  colDefs: ColDef[] = [
+    { headerName: "Make & Model", valueGetter: p => `${p.data.make} ${p.data.model}` },
+    { field: "model" },
+    { field: "price", type: 'currency' },
+    { field: "electric" }
+  ];
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AggridComponent } from './aggrid/aggrid.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AggridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-grids';
+  title = 'Grid experimentation';
 }
