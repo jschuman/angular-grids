e44aba346195a0a9756a34d6222ea62f19c11bb9
Jeff Schuman <jschuman@solutionstreet.com>
2024-05-09 20:00:15 +0000

    client pagination working

--- src/app/aggrid/aggrid.component.ts
+++ src/app/aggrid/aggrid.component.ts
@@ -1,44 +1,62 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
-import { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
+import { ColDef, GridReadyEvent } from 'ag-grid-community'; // Column Definition Type Interface
+import { IOlympicData } from '../../interfaces';
+
 
 @Component({
   selector: 'app-aggrid',
   standalone: true,
-  imports: [AgGridAngular],
+  imports: [AgGridAngular, HttpClientModule],
   templateUrl: './aggrid.component.html',
   styleUrl: './aggrid.component.css'
 })
 
 export class AggridComponent {
+
   // Row Data: The data to be displayed.
-  rowData = [
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-  ];
+  public rowData!: IOlympicData[];
 
-  CurrencyFormatter = (params: { value: any; }) => {
-    return `$${params.value}`;
-  }
-
-  columnTypes = {
-    currency: { 
-        width: 150,
-        valueFormatter: this.CurrencyFormatter
-    },
-  };
-
-  defaultColDef: ColDef = {
-    minWidth: 150,
-    filter: "agTextColumnFilter"
+  public defaultColDef: ColDef = {
+    minWidth: 100,
+    filter: true
   }
 
   // Column Definitions: Defines the columns to be displayed.
-  colDefs: ColDef[] = [
-    { headerName: "Make & Model", valueGetter: p => `${p.data.make} ${p.data.model}` },
-    { field: "model"},
-    { field: "price", type: 'currency' },
-    { field: "electric" }
+  public columnDefs: ColDef[] = [
+    { field: "athlete", minWidth: 200 },
+    {
+      field: "age"
+    },
+    {
+      field: "country",
+      minWidth: 200,
+    },
+    { field: "year" },
+    { field: "sport", minWidth: 200, menuTabs: [] },
+    { field: "gold" },
+    { field: "silver" },
+    { field: "bronze" },
+    { field: "total" },
   ];
+
+  // enables pagination in the grid
+  public pagination = true;
+
+  // sets 10 rows per page (default is 100)
+  public paginationPageSize = 10;
+
+  // allows the user to select the page size from a predefined list of page sizes
+  public paginationPageSizeSelector = [10, 20, 50, 100];
+
+  constructor(private http: HttpClient) {}
+
+  onGridReady(params: GridReadyEvent<IOlympicData>) {
+    this.http
+      .get<
+        IOlympicData[]
+      >("https://www.ag-grid.com/example-assets/olympic-winners.json")
+      .subscribe((data) => (this.rowData = data));
+  }
 }
